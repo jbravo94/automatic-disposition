c3c3a62988d8b17be49c4ee7b3da3d0153397b61
Johannes HEINZL <jh.heinzl@gmail.com>
2023-03-20 00:29:20 +0100

    Updates.

--- api/src/main/java/org/openmrs/module/automaticdisposition/advice/EncounterSaveAdvice.java
+++ api/src/main/java/org/openmrs/module/automaticdisposition/advice/EncounterSaveAdvice.java
@@ -6,8 +6,14 @@ import org.openmrs.Concept;
 import org.openmrs.ConceptMap;
 import org.openmrs.ConceptReferenceTerm;
 import org.openmrs.Encounter;
+import org.openmrs.Location;
 import org.openmrs.Obs;
+import org.openmrs.Patient;
+import org.openmrs.Visit;
+import org.openmrs.VisitType;
 import org.openmrs.api.EncounterService;
+import org.openmrs.api.LocationService;
+import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.automaticdisposition.api.impl.TransactionManager;
 import org.openmrs.module.emrapi.encounter.EmrEncounterService;
@@ -23,6 +29,7 @@ import java.net.URI;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -41,10 +48,16 @@ public class EncounterSaveAdvice implements AfterReturningAdvice {
 	
 	private final EncounterService encounterService;
 	
+	private final VisitService visitService;
+	
+	private final LocationService locationService;
+	
 	public EncounterSaveAdvice() throws SQLException {
 		PlatformTransactionManager platformTransactionManager = getSpringPlatformTransactionManager();
 		transactionManager = new TransactionManager(platformTransactionManager);
-		encounterService = getEncounterService();
+		encounterService = Context.getEncounterService();
+		visitService = Context.getVisitService();
+		locationService = Context.getLocationService();
 	}
 	
 	@Override
@@ -91,6 +104,45 @@ public class EncounterSaveAdvice implements AfterReturningAdvice {
 							
 							if (dispositonLocation != null && dispositonvisitType != null) {
 								LOGGER.error("Move to " + dispositonLocation + " and start visit via " + dispositonvisitType);
+								
+								Patient patient = encounter.getPatient();
+								Visit visit = encounter.getVisit();
+								
+								List<VisitType> allVisitTypes = visitService.getAllVisitTypes();
+								
+								VisitType actualVisitType = null;
+								
+								for (VisitType visitType : allVisitTypes) {
+									if (dispositonvisitType.equals(visitType.getName())) {
+										actualVisitType = visitType;
+										break;
+									}
+								}
+								
+								if (actualVisitType == null) {
+									return;
+								}
+								
+								List<Location> allLocations = locationService.getAllLocations();
+								
+								Location actualLocation = null;
+								
+								for (Location location : allLocations) {
+									if (dispositonLocation.equals(location.getName())) {
+										actualLocation = location;
+										break;
+									}
+								}
+								
+								if (actualLocation == null) {
+									return;
+								}
+								
+								Visit dispositionedVisit = new Visit(patient, actualVisitType, new Date());
+								dispositionedVisit.setLocation(actualLocation);
+								
+								visitService.endVisit(visit, new Date());
+								visitService.saveVisit(dispositionedVisit);
 							}
 						}
 						
@@ -110,14 +162,9 @@ public class EncounterSaveAdvice implements AfterReturningAdvice {
 		}
 	}
 	
-	private EncounterService getEncounterService() {
-		return Context.getEncounterService();
-	}
-	
 	private PlatformTransactionManager getSpringPlatformTransactionManager() {
 		List<PlatformTransactionManager> platformTransactionManagers = Context
 		        .getRegisteredComponents(PlatformTransactionManager.class);
 		return platformTransactionManagers.get(0);
 	}
-	
 }
