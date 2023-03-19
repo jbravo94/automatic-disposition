6042dffcfd96a993334a98753f67bfb31ae37feb
Johannes HEINZL <jh.heinzl@gmail.com>
2023-03-19 21:40:36 +0100

    Working.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Build
+* `mvn -DskipTests clean install`
+
+https://github.com/openmrs/openmrs-module-atomfeed/
+https://github.com/ICT4H/atomfeed
+https://github.com/openmrs/openmrs-module-event
+https://github.com/openmrs/openmrs-module-coreapps
+https://github.com/openmrs/openmrs-core
+https://github.com/ICT4H/openmrs-atomfeed
+https://github.com/openmrs/openmrs-module-emrapi
+https://wiki.openmrs.org/display/docs/Event+Module
+https://wiki.openmrs.org/display/docs/Create+and+Deploy+Your+First+OpenMRS+Module
+https://wiki.openmrs.org/display/docs/Set+Up+OpenMRS+Server+with+OpenMRS+SDK+and+Docker#SetUpOpenMRSServerwithOpenMRSSDKandDocker-InstallOpenMRSSDK
+https://guide.openmrs.org/en/Configuration/customizing-openmrs-with-plug-in-modules.html
--- api/src/main/java/org/openmrs/module/automaticdisposition/advice/EncounterSaveAdvice.java
+++ api/src/main/java/org/openmrs/module/automaticdisposition/advice/EncounterSaveAdvice.java
@@ -1,11 +1,18 @@
 package org.openmrs.module.automaticdisposition.advice;
 
 import org.apache.commons.beanutils.PropertyUtils;
+import org.ict4h.atomfeed.transaction.AFTransactionWorkWithoutResult;
+import org.openmrs.Encounter;
+import org.openmrs.api.EncounterService;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.automaticdisposition.api.impl.TransactionManager;
+import org.openmrs.module.emrapi.encounter.EmrEncounterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.AfterReturningAdvice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.openmrs.api.context.Context;
 
 import java.lang.reflect.Method;
 import java.net.URI;
@@ -24,16 +31,50 @@ public class EncounterSaveAdvice implements AfterReturningAdvice {
 	
 	private static final String SAVE_METHOD = "save";
 	
+	private final TransactionManager transactionManager;
+	
+	private final EncounterService encounterService;
+	
 	public EncounterSaveAdvice() throws SQLException {
+		PlatformTransactionManager platformTransactionManager = getSpringPlatformTransactionManager();
+		transactionManager = new TransactionManager(platformTransactionManager);
+		encounterService = getEncounterService();
 	}
 	
 	@Override
 	public void afterReturning(Object returnValue, Method method, Object[] args, Object emrEncounterService)
 	        throws Throwable {
 		if (method.getName().equals(SAVE_METHOD)) {
-			Object encounterUuid = PropertyUtils.getProperty(returnValue, "encounterUuid");
+			Object encounterUuidObject = PropertyUtils.getProperty(returnValue, "encounterUuid");
+			
+			String encounterUuid = encounterUuidObject.toString();
+			
+			transactionManager.executeWithTransaction(new AFTransactionWorkWithoutResult() {
+				
+				@Override
+				protected void doInTransaction() {
+					Encounter encounter = encounterService.getEncounterByUuid(encounterUuid);
+					LOGGER.error(encounter.toString());
+				}
+				
+				@Override
+				public PropagationDefinition getTxPropagationDefinition() {
+					return PropagationDefinition.PROPAGATION_REQUIRED;
+				}
+			});
+			
 			LOGGER.error(encounterUuid.toString());
 		}
+	}
+	
+	private EncounterService getEncounterService() {
+		return Context.getEncounterService();
+	}
+	
+	private PlatformTransactionManager getSpringPlatformTransactionManager() {
+		List<PlatformTransactionManager> platformTransactionManagers = Context
+		        .getRegisteredComponents(PlatformTransactionManager.class);
+		return platformTransactionManagers.get(0);
 	}
 	
 }
